4aa08a13f9867d2f55ba693741973d6a6bf303cc
cnxk <qqpass@163.com>
2019-10-22 18:07:50 +0800

    init

---
+++ src/main/java/com/springboot/test/Application.java
@@ -0,0 +1,19 @@
+package com.springboot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ *  <p>
+ *  测试类入口
+ *  </p>
+ * @version v1.0.0
+ * @date 2019年10月22日 下午5:39:48
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
